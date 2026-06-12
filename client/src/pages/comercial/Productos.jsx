4695873f43ce1c63d3cb6805576df10aa30d5305
Picasso017 <matiaspicasso017@gmail.com>
2026-06-12 20:31:43 -0300

    asd

--- client/src/pages/comercial/Productos.jsx
+++ client/src/pages/comercial/Productos.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import FormularioProductos from "../../components/FormularioProductos";
+import axios from "axios";
+
 
 export default function Productos() {
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
+  const [producto, setProducto] = useState([])
+
+  axios.get('http://localhost:3000/products',{withCredentials:true})
+            .then((Response)=> setProducto(Response.data))
 
   return (
     <section className="page-shell">
       <h1>Productos</h1>
       <p>Administra el catálogo de productos para tu clínica veterinaria.</p>
 
+    
       <button
         type="button"
         onClick={() => setMostrarFormulario((prev) => !prev)}
       >
         {mostrarFormulario ? "Ocultar formulario" : "Agregar producto"}
       </button>
+
 
       {mostrarFormulario && <FormularioProductos />}
     </section>
